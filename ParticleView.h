db0f339ad65b29c9eb8761ee898a50e65afcbdc6
Jill Pestka <info@twinyvine.com>
2013-08-08 12:19:14 -0400

    Final Version
    Added a fun animation to the FirstViewController

---
+++ ParticleView.h
@@ -0,0 +1,13 @@
+//
+//  ParticleView.h
+//  TeslaWalls
+//
+//  Created by Jill Pestka on 8/8/13.
+//  Copyright (c) 2013 Jill Pestka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParticleView : UIView
+
+@end
---
+++ ParticleView.m
@@ -0,0 +1,31 @@
+//
+//  ParticleView.m
+//  TeslaWalls
+//
+//  Created by Jill Pestka on 8/8/13.
+//  Copyright (c) 2013 Jill Pestka. All rights reserved.
+//
+
+#import "ParticleView.h"
+
+@implementation ParticleView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
